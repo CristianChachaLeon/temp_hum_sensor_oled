726d77b6d5de7fe53f7ee3067f4a172d70d441b6
chacha <cristianchachaleon@gmail.com>
2024-01-25 18:46:12 +0100

    Added wrapper for ssd1306 oled, show temp and humidity data also modify bme280 wrapper to return temp and humidity

---
+++ lib/HMI_oled/HMI_oled.c
@@ -0,0 +1,22 @@
+#include "HMI_oled.h"
+
+void set_Font_oled(const uint8_t *progmemFont)
+{
+    ssd1306_setFixedFont(progmemFont);
+}
+
+void init_oled(void){
+    ssd1306_128x64_i2c_init();
+    ssd1306_clearScreen();
+}
+
+void show_temp_and_hum_oled(int32_t temp , uint32_t humidity){
+
+    char data_buffer_temp[10];
+    char data_buffer_hum[10];
+    snprintf(data_buffer_temp , sizeof(data_buffer_temp),"%ld",temp);
+    snprintf(data_buffer_hum , sizeof(data_buffer_hum),"%ld",humidity);
+
+    ssd1306_printFixed(35,5 , data_buffer_temp, STYLE_NORMAL);
+    ssd1306_printFixed(35,35, data_buffer_hum, STYLE_NORMAL);
+}
--- src/main.c
+++ src/main.c
@@ -3,36 +3,36 @@
 #include <stdio.h>
 #include "uart.h"
 #include "bme280_wrapper.h"
-#include "ssd1306.h"
+#include "HMI_oled.h"
 
 int main(void)
 {
-  init_uart(115200);
-  i2c_init();
   int32_t temp=0;
-  char data_buffer[10];
+  uint32_t humidity=0;
   int8_t rslt = 0;
   uint32_t period;
   struct bme280_dev dev;
   struct bme280_settings settings;
+  
+  init_uart(115200);
+  i2c_init();
+  
   bme280_config_and_init(&settings, &dev, &period);
-  ssd1306_setFixedFont(ssd1306xled_font8x16);
-  ssd1306_128x64_i2c_init();
-  ssd1306_clearScreen();
+  set_Font_oled(comic_sans_font24x32_123);
+  init_oled();
+  
   
   printf("Display information: size %d x %d\n",ssd1306_displayWidth(),ssd1306_displayHeight());
 
   while (1)
   {
-    temp++;
-    rslt = get_temperature(period, &dev);
-    //rslt = get_temperature(period, &dev,temp);
-    //printf("data temp : %d\n",temp);
+    rslt = get_temperature(period, &dev,&temp);
     bme280_error_codes_print_result("bme280_get_temperature", rslt);
-    rslt = get_humidity(period, &dev);
+
+    rslt = get_humidity(period, &dev,&humidity);
     bme280_error_codes_print_result("bme280_get_humidity", rslt);
-    snprintf(data_buffer , sizeof(data_buffer),"%ld",temp);
-    ssd1306_printFixed(0,10, data_buffer, STYLE_NORMAL);
+
+    show_temp_and_hum_oled(temp,humidity);
     _delay_ms(1000);
   }
 
