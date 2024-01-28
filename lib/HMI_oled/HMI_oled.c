f2132ac2d9b57a8313923e86a8e23e0190b8cd3c
chacha <cristianchachaleon@gmail.com>
2024-01-28 16:46:56 +0100

    Added temperature and humidity printing in Oled screen

--- lib/HMI_oled/HMI_oled.c
+++ lib/HMI_oled/HMI_oled.c
@@ -15,8 +15,8 @@ void show_temp_and_hum_oled(int32_t temp , uint32_t humidity){
     char data_buffer_temp[10];
     char data_buffer_hum[10];
     snprintf(data_buffer_temp , sizeof(data_buffer_temp),"%ld",temp);
-    snprintf(data_buffer_hum , sizeof(data_buffer_hum),"%ld",humidity);
-
-    ssd1306_printFixed(35,5 , data_buffer_temp, STYLE_NORMAL);
+    snprintf(data_buffer_hum , sizeof(data_buffer_hum),"%ld%%",humidity);
+    ssd1306_drawLine(0,ssd1306_displayHeight()/2, ssd1306_displayWidth(), ssd1306_displayHeight()/2);
+    ssd1306_printFixed(35,0 , data_buffer_temp, STYLE_ITALIC);
     ssd1306_printFixed(35,35, data_buffer_hum, STYLE_NORMAL);
 }
